f9474e6b6c75f67adb5edd3d5c66d65e0be5b4c1
Blaarsito <Fusionblur17@gmail.com>
2018-03-18 01:33:36 +0100

    Primer programa 'adivina el numero'. if, else, int y float.

---
+++ adivina_numero.py
@@ -0,0 +1,12 @@
+
+
+
+if leche_en_la_nevera:
+    hacer_colacao()
+else:
+    compramos_leche()
+
+user_number = int(input('dime un numero'))
+
+if user_number > 5:
+    print('tu numero es mayor que 5')
