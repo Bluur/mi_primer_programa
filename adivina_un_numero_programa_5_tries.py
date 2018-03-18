52033211816c890d42fac918a65ce65d3415ef45
Blaarsito <Fusionblur17@gmail.com>
2018-03-18 03:47:06 +0100

    Programa de adivinar el numero, con 5 intentos.

---
+++ adivina_un_numero_programa_5_tries.py
@@ -0,0 +1,26 @@
+
+number_to_guess = 10
+
+user_number = int(input('Adivina un numero: '))
+
+if user_number == number_to_guess:
+    print('Has ganado')
+else:
+    print('Intentalo de nuevo')
+user_number = int(input('Adivina un numero: '))
+if user_number == number_to_guess:
+    print('Has ganado')
+else:
+    print('Intentalo de nuevo')
+user_number = int(input('Adivina un numero: '))
+if user_number == number_to_guess:
+    print('Has ganado')
+else:
+    print('Intentalo de nuevo')
+user_number = int(input('Adivina un numero: '))
+if user_number == number_to_guess:
+    print('Has ganado')
+else:
+    print('Has perdido')
+
+
