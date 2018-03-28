55741d3c8bca98ad770db484070b648bd70cdaf9
Blaarsito <Fusionblur17@gmail.com>
2018-03-28 14:31:19 +0200

    Jugando con el for, in y las listas :D

--- mas_grande_de_los_10.py
+++ mas_grande_de_los_10.py
@@ -3,3 +3,16 @@ Pregunta al usuario una serie de 10 numeros, determina cual es el mas grande de 
 """
 
 numeros_usuario = []
+input_usuario = ""
+
+while input_usuario != "FIN":
+    input_usuario = input("Dime los numeros que deseas comparar: ").capitalize()
+    if input_usuario != "FIN":
+        numeros_usuario.append(input_usuario)
+
+
+
+
+
+
+
